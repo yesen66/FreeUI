275f4a427cd5252dbd24f1f1698203698866b368
sen Ye <654923922@qq.com>
2023-10-04 11:26:53 +0800

    feat: 删除不必要代码

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import Button, { ButtonSize, ButtonType } from './components/Button/button';
 import Input from './components/Input/input';
 import Menu from './components/Menu/menu';
@@ -12,25 +11,13 @@ library.add(fas)
 
 
 function App() {
-  // const lakes = [
-  //   'bradley', 'pope', 'caruso', 'cook', 'cousins',
-  //   'jams', 'AD', 'green', 'howard', 'kuzma', 'McGee', 'rando'
-  // ]
-  // const handleFetch = (query: string) => {
-  //   return lakes.filter(name => name.includes(query))
-  // }
-
   return (
     <div className="App">
-      {/* <AutoComplete
-        fetchSuggestions={handleFetch}
-      /> */}
       <Button className='yes'>hello</Button>
       <Button disabled> Disabled Button </Button>
       <Button autoFocus onClick={() => alert(1)} btnType={ButtonType.Primary} size={ButtonSize.Small} >hello</Button>
       <Button btnType={ButtonType.Danger} size={ButtonSize.Large}>hello</Button>
       <Button btnType={ButtonType.Link} href='http://www.baidu.com' target="_blank">baidu</Button>
-      <Button btnType={ButtonType.Link} href='http:www.baidu.com'>baidu</Button>
       <Input disabled={true} ></Input>
 
       <Menu defaultIndex='0'>
@@ -54,29 +41,6 @@ function App() {
       </Menu>
       <Upload
         action="https://jsonplaceholder.typicode.com/posts"
-      />
-      <Alert
-        title='123'
-        type={AlertType.Default}
-        className='myAlert'
-      />
-      <Alert
-        title='123'
-        type={AlertType.Warning}
-        className='myAlert'
-      />
-      <Alert
-        title='123'
-        type={AlertType.Success}
-        className='myAlert'
-        description='hah '
-      />
-      <Alert
-        title='124'
-        type={AlertType.Danger}
-        description='我的'
-        closable={true}
-        onClose={() => { console.log(1) }}
       />
     </div>
   );
