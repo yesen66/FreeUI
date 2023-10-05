4a862a448405916c093a7a322ec84f093a462a39
sen Ye <654923922@qq.com>
2023-10-05 15:27:18 +0800

    feat: 修改包名

--- src/components/Input/input.tsx
+++ src/components/Input/input.tsx
@@ -19,7 +19,7 @@ export interface InputProps extends Omit<InputHTMLAttributes<HTMLElement>, 'size
  * 
  * ~~~js
  * // 这样引用
- * import { Input } from 'freeui'
+ * import { Input } from 'yefreeui'
  * ~~~
  * 
  * 支持 HTMLInput 的所有基本属性
